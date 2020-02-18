4aeb3700ca2328bfc990ae1a887f3d2a8003f792
manunand <rc.anandkammar@gmail.com>
2020-02-18 20:10:43 +0530

    first commit

---
+++ navbar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<?php 
+		include 'links.php';
+	?>
+</head>
+<body>
+	<nav class="navbar navbar-inverse">
+		 <div class="container-fluid">
+		 	<div class="navbar-header">
+		 		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+		 			<span class="icon-bar"></span>
+		 			<span class="icon-bar"></span>
+		 			<span class="icon-bar"></span>
+		 		</button>
+		 		<a class="nabar-brand" href="#"><img src="img/w3newbie.png"></a>
+		 	</div>
+		 	<div class="collapse navbar-collapse" id="myNavbar">
+		 		<ul class="nav navbar-nav navbar-right">
+		 			<li class="active"><a href="#">Home</a></li>
+		 			<li><a href="#">Gallery</a></li>
+		 			<li><a href="#">Administration</a></li>
+		 			<li><a href="#">Login</a></li>
+		 			<li><a href="#">About</a></li>
+		 		</ul>
+		 	</div>
+		 </div>
+	</nav>
+</body>
+</html>
