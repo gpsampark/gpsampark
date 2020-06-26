eafa38b3184355b0808540a46b0aefcf5414c926
gpsampark <gpsampark@gmail.com>
2020-06-26 10:54:27 +0530

    edit expenditure completed

--- zpadmin/editdata.php
+++ zpadmin/editdata.php
@@ -35,26 +35,28 @@
 						?>
 						<table class="table table-bordered" style="color: black; font-weight: bold;">
 							<thead  style="text-align: center;">
-								<tr><td colspan="7">Monthly Progress Report of Projects implemented under Mahatma Gandhi National Rural Employment Scheme</td></tr>
-								<tr><td colspan="2"></td><td colspan="5">Rupees in Lakhs</td></tr>
-								<tr><td rowspan="3">Sl.No</td><td rowspan="3">Project Name</td><td colspan="5">Progress until</td></tr>
-								<tr><td colspan="2">Processing</td><td colspan="2">Completed</td><td rowspan="2">Total Expenditure(Rupees in Lakhs)</td></tr>
+								<tr><td colspan="6">Monthly Progress Report of Projects implemented under Mahatma Gandhi National Rural Employment Scheme</td></tr>
+								<tr><td colspan="2"></td><td colspan="4">Rupees in Lakhs</td></tr>
+								<tr><td rowspan="3">Sl.No</td><td rowspan="3">Project Name</td><td colspan="4">Progress until</td></tr>
+								<tr><td colspan="2">Processing</td><td colspan="2">Completed</td></tr>
 								<td>Remaining(Cont projects)</td><td>Current(New Projects)</td><td>Remaining(Cont. projects)</td><td>Current(New Projects)</td>
 							</thead>
 							<tbody>
 						<?php
-						$i=0;
+						$i = -1;
 						while ($row= $result-> fetch_assoc()) {
-							$total=$row['rem_processing']+$row['rem_completed']+$row['cur_processing']+$row['cur_completed'];
+							$i++;
+							
 							?>
 							<tr><td><?php echo $row['slno']; ?></td><td><?php echo $row['project_name']; ?></td>
-								<td><input type="Number" value="<?php echo "".$row['rem_processing']."" ?>" name="rem_processing[<?php echo $i; ?>]" required="" id="rem_processing[<?php echo $i; ?>]"></td>
-								<td><input type="Number" value="<?php echo "".$row['cur_processing']."" ?>" name="rem_processing[<?php echo $i; ?>]" required="" id="cur_processing[<?php echo $i; ?>]"></td>
-								<td><input type="Number" value="<?php echo "".$row['rem_completed']."" ?>" name="rem_processing[<?php echo $i; ?>]" required="" id="rem_completed[<?php echo $i; ?>]"></td>
-								<td><input type="Number" value="<?php echo "".$row['cur_completed']."" ?>" name="rem_processing[<?php echo $i; ?>]" required="" id="cur_completed[<?php echo $i; ?>]"></td>
+								<td><input type="hidden" value="<?php echo "".$row['exp_id']."" ?>" name="expid[<?php echo $i; ?>]" id="expid[<?php echo $i; ?>]">
+									<input type="hidden" value="<?php echo "".$i."" ?>" name="i" id="i">
+									<input type="Number" value="<?php echo "".$row['rem_processing']."" ?>" name="rem_processing[<?php echo $i; ?>]" required="" id="rem_processing[<?php echo $i; ?>]"></td>
+								<td><input type="Number" value="<?php echo "".$row['cur_processing']."" ?>" name="cur_processing[<?php echo $i; ?>]" required="" id="cur_processing[<?php echo $i; ?>]"></td>
+								<td><input type="Number" value="<?php echo "".$row['rem_completed']."" ?>" name="rem_completed[<?php echo $i; ?>]" required="" id="rem_completed[<?php echo $i; ?>]"></td>
+								<td><input type="Number" value="<?php echo "".$row['cur_completed']."" ?>" name="cur_completed[<?php echo $i; ?>]" required="" id="cur_completed[<?php echo $i; ?>]"></td>
 							</tr>
 							<?php
-							$i++;
 						}
 						echo "</tbody></table>";?>
 						<div class="container" align="center">
--- zpadmin/editdatascript.php
+++ zpadmin/editdatascript.php
@@ -22,7 +22,32 @@
 				die("Connection failed: ");
 			}
 			if (isset($_POST['submit'])) {
-				
+				$i=$_POST['i'];
+				$expid=$_POST['expid'];
+				$rem_processing=$_POST['rem_processing'];
+				$cur_processing=$_POST['cur_processing'];
+				$rem_completed=$_POST['rem_completed'];
+				$cur_completed=$_POST['cur_completed'];
+				$j=0;
+				$flag=0;
+				while ($j<=$i) {
+					
+					$eid=$expid[$j];
+					$rp=$rem_processing[$j];
+					$cp=$cur_processing[$j];
+					$rc=$rem_completed[$j];
+					$cc=$cur_completed[$j];
+							
+					$query="UPDATE expenditure SET rem_processing = '".$rp."', cur_processing = '".$cp."', rem_completed = '".$rc."', cur_completed = '".$cc."' WHERE exp_id = '".$eid."';";
+					$res = mysqli_query($conn,$query);
+					$j=$j+1;
+				}
+				?>
+					<div class="alert alert-success">
+						<strong>Success!</strong> Your response has been successfully recorded.Thank you.
+						<meta http-equiv="refresh" content="2;url=editexpenditure.php" />
+					</div>
+				<?php
 			}
 		?>
 	</header>
